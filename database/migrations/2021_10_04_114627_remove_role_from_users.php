8af0b0ee44f283ba11fa8698630bf3cd61a7257d
TaitXtended <marveloustchimusoro@gmail.com>
2021-10-04 13:54:27 +0200

    removed role1

--- database/migrations/2021_10_04_114627_remove_role_from_users.php
+++ database/migrations/2021_10_04_114627_remove_role_from_users.php
@@ -26,7 +26,7 @@ class RemoveRoleFromUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 }
